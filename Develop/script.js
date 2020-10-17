e835520c8fc73f5915c8e0669fa453c141e5ac18
Kyle Galer <chembot81@hotmail.com>
2020-10-17 18:16:22 -0500

    clean up

--- Develop/script.js
+++ Develop/script.js
@@ -1,85 +1,69 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// prettier-ignore
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// prettier-ignore
+let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// prettier-ignore
+let specialCharacters = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '?', '@', ',', '-', '.', '/', ':', ';', '<', '>', '=', '[', ']', '\\', '^', '_', '`', '{', '}', '|', '~'];
+// prettier-ignore
 
-let lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let number = ["0","1","2","3","4","5","6","7","8","9"];
-let special = ["!", "\"", "#", "$", "%","^","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","]","[","\\","^","_","`","{","|","}","~"];
-let arrNum;
-let temp;
+let randomPassword = [];
+
+// generate random password 
+function generatePassword(arr, number) {
+	let newArray = arr.flat();
+	let newPassword = [];
+	for (let i = 0; i < number; i++) {
+		let randomIndex = Math.floor(Math.random() * newArray.length - 1) + 1;
+		newPassword.push(newArray[randomIndex]);
+	}
+	return newPassword.join('');
+}
+
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+	let numberOfCharacters = parseInt(prompt('How many characters do you want the password to be?'));
 
-} 
-function generatePassword() {
-  let length = prompt("Select a password length between 8 and 128 characters.");
-  if (length > 128) {
-    alert("This is an invalid selection. Please select again.");
-    reload();
-  }
-  if (length < 8) {
-    alert ("This is an invalid selection. Please select again.")
-    reload();
-  }
-  let lowerPrompt = confirm("Would you like to include lowercase letters in your password?");
-  let upperPrompt = confirm("Would you like to include uppercase letters in your password?");
-  let numberPrompt = confirm("Would you like to include numbers in your password?");
-  let specialPrompt = confirm("Would you like to include special characters in your password?");
+	if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+		alert('Please type a number between 8 and 128.');
+		return;
+	} else if (isNaN(numberOfCharacters)) {
+		alert('Please type a valid number.');
+		return;
+	}
 
-  console.log(length);
-  console.log(lowerPrompt);
-  let arrChoices = 4
-  let charNum = Math.ceil(length / arrChoices);
-  console.log(charNum)
-  
-  // if lowerPrompt = false arrChoices--
-  // if lowerPrompt = true loop randoNum charNum of times
-  for (i = 0; i < charNum; i++) {
-    randoNum(lower)
-    temp = []
-    temp.push(arrNum)
-  // concat() to join two or more arrays
-  // Math.random with concatenated arrays
-  } 
-  
+	let specialCharactersQuestion = confirm('Do you want special characters? Click Ok for yes, cancel for no.');
 
+	let numberQuestion = confirm('Do you want the password to have numbers? Click Ok for yes, cancel for no.');
+
+	let lowerCaseQuestion = confirm('Do you want lowercase characters? Click Ok for yes, cancel for no.');
+
+	let upperCaseQuestion = confirm('Do you want uppercase characters? Click Ok for yes, cancel for no.');
+
+
+	if (specialCharactersQuestion) {
+		randomPassword.push(specialCharacters);
+	}
+
+	if (numberQuestion) {
+		randomPassword.push(numbers);
+	}
+
+	if (lowerCaseQuestion) {
+		randomPassword.push(lowerCase);
+	}
+
+	if (upperCaseQuestion) {
+		randomPassword.push(upperCase);
+	}
+
+	else if (!specialCharactersQuestion && !numberQuestion && !lowerCaseQuestion && !upperCaseQuestion) {
+		alert('Please select at least one character type.')
+		return;
+	}
+
+	let password = generatePassword(randomPassword, numberOfCharacters);
+
+	passwordText.value = password;
 }
-
-function randoNum(lower) {
-  let indexNum = Math.floor(Math.random() * lower.length);
-  arrNum = lower[indexNum];
-  console.log(randoNum)
-}
-// function randoNum(lower) {
-  // let indexNum = Math.floor(Math.random() * arr. length);
-  // arrNum = arr[indexNum];
-function randoNum(upper) {
-  let indexNum = Math.floor(Math.random() * upper.length);
-}
-function randoNum(number) {
-  
-}
-randoNum(lower);
-console.log(arrNum);
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Click button to generate password.
-
-// Create prompts for password criteria, next.
-
-// When prompted, I select which password criteria to include.
-
-// When prompted for length of password it needs to be 8 to 128 characters.
-
-// Prompt for lowercase, uppercase, numerical, and special characters.
-
-// At least one of these must be selected.
-
-// When all prompts input, a password is generated.
-
-// Create an alert that displays the password.
